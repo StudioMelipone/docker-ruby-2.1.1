fbbbfa9c9694bd9eb5d6e8e9a681490382f640e8
Loïc Guitaut <loic@studiomelipone.eu>
2014-02-26 13:40:50 +0100

    Change path where bundler installs gems

--- rails.sh
+++ rails.sh
@@ -1,6 +1,6 @@
-#!/bin/bash -l
+#!/bin/sh
 cd /rails_app
-bundle install --path /rails_app/docker/bundle
+bundle install --path ./docker/bundle
 bundle exec rake db:create
 bundle exec rake db:migrate
 /rails_app/docker/run.sh
